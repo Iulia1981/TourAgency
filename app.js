d0025c5efb5600bf3234cf30178b7007f3d89b10
Iulia1981 <hvaldinajulia@yahoo.com>
2016-11-28 13:56:56 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -13,10 +13,13 @@ var app = express();
 //создаем порт
 app.set('port', process.env.PORT || 3000);
 
-
+app.use(express.static(__dirname + '/public'));
 // view engine setup
+
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -26,25 +29,39 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+//app.use('/', index);
+//app.use('/users', users);
 
+//маршруты для домашней страницы 
+app.get('/', function(req, res){
+  res.render('home');
+});
+
+var fortunes = [
+  "Победи свои страхи, или они победят тебя.",
+  "Рекам нужны истоки.",
+  "Не бойся неведомого.",
+  "Тебя ждет приятный сюрприз.",
+  "Будь проще везде, где только можно."
+];
+
+//маршруты для страницы About
+app.get('/about', function (req,res){
+  var randomFortune =
+      fortunes[Math.floor(Math.random() * fortunes.length)];
+  res.render('about', { fortune: randomFortune });
+});
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+  res.status(404);
+  res.render('404');
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  console.error(err.stack);
+  res.status(500);
+  res.render('500');
 });
 
 app.listen(app.get('port'), function () {
